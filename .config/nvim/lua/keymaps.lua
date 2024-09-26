fef8799d435b31ee2296d39de370adc535f9c7cd
AhmedOsman101 <trollingosman101@outlook.com>
2024-09-26 17:39:35 +0300

    update 1 file and create 1 file

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,3 +2,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.keymap.set("n", "<leader>w", ":w<CR>") -- Save file
+vim.keymap.set("n", "<leader>q", ":q<CR>") -- Quite file
+vim.keymap.set("n", "<leader>wq", ":wq<CR>") -- Save and quite file
+
