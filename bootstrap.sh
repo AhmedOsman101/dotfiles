d90637ddbb5f1e92fae1468fa589d3cff532c44c
AhmedOsman101 <trollingosman101@outlook.com>
2024-11-05 22:55:23 +0200

    Updated Files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,31 @@
 #!/usr/bin/env bash
 
+set +e # Disable exit on error
+
+# Install nala
+sudo apt install nala -y
+
+# Install nala packages
+while IFS= read -r line; do
+  sudo nala install "$line" -y
+done <"installed_packages.txt"
+
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Go to the .dotfiles directory and install your homebrew packages
-cd ~/.dotfiles && brew bundle
+# Install homebrew packages
+brew bundle
 
-# Go back to your previous working directory
-cd - || exit
+# Install pnpm
+curl -fsSL https://get.pnpm.io/install.sh | sh -
 
+# Install pnpm packages
+while IFS= read -r line; do
+  pnpm add -g "$line"
+done <"pnpm_global_packages.txt"
+
+# Insatll flatpak apps
+while IFS= read -r line; do
+  # Install nala packages
+  flatpak install "$line" -y
+done <"flatpak_apps.txt"
