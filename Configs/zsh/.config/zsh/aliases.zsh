35a87ac21bcc7116a85d47a8fecca39a0db0df8f
AhmedOsman101 <trollingosman101@outlook.com>
2025-02-19 03:40:02 +0200

    Updated Files 2025-02-19@03:40AM

--- Configs/zsh/.config/zsh/aliases.zsh
+++ Configs/zsh/.config/zsh/aliases.zsh
@@ -49,3 +49,9 @@ alias psmem='ps aux | sort -nr -k 4 | head -5 | fzf'
 # ---- get top process eating cpu ---- #
 alias pscpu='ps aux | sort -nr -k 3 | head -5 | fzf'
 alias git_current_branch='git rev-parse --abbrev-ref HEAD'
+
+# ---- quickly navigate to my dotfiles ---- #
+alias dotfiles="cd $TUCKR_DIR"
+
+# ---- markdown previewer ---- #
+# alias glow="glow -p" # pagination mode
