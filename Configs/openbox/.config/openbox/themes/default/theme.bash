2f4240edd6aea0eb23fa1ef62755b215dd9f5106
AhmedOsman101 <trollingosman101@outlook.com>
2025-02-22 02:13:45 +0200

    (chore): changed cursor theme and openbox config
    - Changed the Cursor theme to Bibata system wide
    - Added the Super + Alt + l keybind to lock screen
    - Commented out dm-hub script shortcut
    - Added a new rule for Alacritty to start at the center of the screen

--- Configs/openbox/.config/openbox/themes/default/theme.bash
+++ Configs/openbox/.config/openbox/themes/default/theme.bash
@@ -51,7 +51,7 @@ geany_font='JetBrainsMono Nerd Font 10'
 gtk_font='Noto Sans 9'
 gtk_theme='Arc-Dark'
 icon_theme='Zafiro'
-cursor_theme='Qogirr'
+cursor_theme='Bibata'
 
 # Openbox
 ob_theme='OB-Arc-Dark'
