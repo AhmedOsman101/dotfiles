acd1fa81db82808476ccb8259feb2677c7a0585d
AhmedOsman101 <trollingosman101@outlook.com>
2025-03-21 17:48:13 +0200

    feat: add jk/kj mappings in insert mode
    - Map jk to escape insert mode
    - Map kj to enter visual mode

--- Configs/nvim/.config/nvim/lua/keymaps.lua
+++ Configs/nvim/.config/nvim/lua/keymaps.lua
@@ -7,3 +7,8 @@ vim.keymap.set("n", "<leader>w", ":w<CR>") -- Save file
 vim.keymap.set("n", "<leader>q", ":q<CR>") -- Quite file
 vim.keymap.set("n", "<leader>wq", ":wq<CR>") -- Save and quite file
 
+-- Map `jk` to enter normal mode from insert mode
+vim.keymap.set("i", "jk", "<Esc>")
+
+-- Map `kj` to enter visual mode from insert mode
+vim.keymap.set("i", "kj", "v")
