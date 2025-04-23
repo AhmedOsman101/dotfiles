ca6d7cc03d044f73ec7f376350e30f54e0713d7d
AhmedOsman101 <trollingosman101@outlook.com>
2025-04-23 17:43:05 +0200

    feat: Add tokei alias and update biome config
    - Added `code-stats` alias for tokei
    - Updated `useTemplate` in biome config

--- Configs/zsh/.config/zsh/aliases.zsh
+++ Configs/zsh/.config/zsh/aliases.zsh
@@ -104,3 +104,6 @@ alias n="nu --config $NU_CONFIG -c"
 # ---- Capslock ---- #
 alias caps="xdotool key Caps_Lock"
 alias CAPS="xdotool key Caps_Lock"
+
+# ---- Tokei ---- #
+alias code-stats='tokei'
