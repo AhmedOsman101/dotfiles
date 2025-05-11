06669280ae6fc1211dc6b542885d8524ebd8671d
AhmedOsman101 <trollingosman101@outlook.com>
2025-05-11 15:21:10 +0300

    create 6 files, delete 15 files and update 84 files

--- Configs/openbox/.config/openbox/themes/launch-bar.sh
+++ Configs/openbox/.config/openbox/themes/launch-bar.sh
@@ -3,19 +3,19 @@
 ## Copyright (C) 2020-2024 Aditya Shakya <adi1090x@gmail.com>
 
 ## Files and Directories
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PFILE="$DIR/.panel"
 
 ## Launch polybar or tint2 accordingly
 launch_bar() {
-	CPANEL="$(cat "$PFILE")"
+	CPANEL="`cat $PFILE`"
 
 	if [[ "$CPANEL" == 'polybar' ]]; then
 		bash "$DIR"/polybar.sh
 	elif [[ "$CPANEL" == 'tint2' ]]; then
 		bash "$DIR"/tint2.sh
 	else
-		bash "$DIR"/polybar.sh
+		bash "$DIR"/polybar.sh	
 	fi
 }
 
