aa7be978112ae49cb8bc3046fc8750a14bce075f
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-09-23 02:30:37 +0300

    Updated Files 2025-09-23@02:30AM

--- Configs/openbox/.config/openbox/themes/tint2.sh
+++ Configs/openbox/.config/openbox/themes/tint2.sh
@@ -7,14 +7,14 @@ STYLE="default"
 
 ## Launch tint2 with selected theme
 launch_bar() {
-	# Terminate already running bar instances
-	killall -q tint2
+  # Terminate already running bar instances
+  killall -q tint2
 
-	# Wait until the processes have been shut down
-	while pgrep -u $UID -x tint2 >/dev/null; do sleep 1; done
+  # Wait until the processes have been shut down
+  while pgrep -u $UID -x tint2 >/dev/null; do sleep 1; done
 
-	# Launch the bar
-	tint2 -c "$HOME"/.config/openbox/themes/"$STYLE"/tint2/tint2rc &
+  # Launch the bar
+  tint2 -c "$HOME"/.config/openbox/themes/"$STYLE"/tint2/tint2rc &
 }
 
 # Execute function
