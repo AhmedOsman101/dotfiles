78ca4b28ddecbe71eb5a7902379085ebef95fe5e
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-10-11 07:19:28 +0300

    Updated Files 2025-10-11@07:19AM

--- Configs/thunar/.config/Thunar/accels.scm
+++ Configs/thunar/.config/Thunar/accels.scm
@@ -1,6 +1,7 @@
 ; thunar GtkAccelMap rc-file         -*- scheme -*-
 ; this file is an automated accelerator map dump
 ;
+; (gtk_accel_path "<Actions>/ThunarDetailsView/expandable-folders" "")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-by-type" "")
 ; (gtk_accel_path "<Actions>/ThunarStatusBar/toggle-last-modified" "")
 ; (gtk_accel_path "<Actions>/Thunarwindow/menu" "")
@@ -93,6 +94,7 @@
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-as-icons" "<Primary>1")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/delete-2" "<Shift>Delete")
 ; (gtk_accel_path "<Actions>/ThunarWindow/zoom-in" "<Primary>plus")
+; (gtk_accel_path "<Actions>/ThunarStandardView/configure-columns" "")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/rename" "F2")
 ; (gtk_accel_path "<Actions>/ThunarBookmarks/41ab0f9c39dc9366cc59e03217e5f979" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/open-location" "<Primary>l")
