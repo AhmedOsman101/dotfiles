59b8e48a10ae37d48bc9058461a4f41ba5f4c5a1
Ahmad Othman <ahmad.ali.othman@outlook.com>
2026-01-07 15:10:54 +0200

    feat(shell): configure shell formatters and update zsh plugins
    - add shellfmt formatter for bash
    - update zsh plugins with comments and formatting
    - improve readability of zsh functions

--- Configs/zsh/.config/zsh/functions.sh
+++ Configs/zsh/.config/zsh/functions.sh
@@ -37,7 +37,14 @@ which() {
   (
     alias
     declare -f
-  ) | /usr/bin/which --tty-only --read-alias --read-functions --show-tilde --show-dot "$@" | shellfmt
+  ) | /usr/bin/which \
+    --tty-only \
+    --read-alias \
+    --read-functions \
+    --show-tilde \
+    --show-dot \
+    "$@" |
+    shellfmt
 }
 
 gif() {
