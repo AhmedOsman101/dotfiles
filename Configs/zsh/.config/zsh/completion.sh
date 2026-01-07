faf6f144ad3f97f4e1231872d852a2a48ccfc95a
Ahmad Othman <ahmad.ali.othman@outlook.com>
2026-01-07 21:24:09 +0200

    chore(zsh): enhance completion configuration
    - enable case-insensitive tab completion
    - improve fzf-tab preview for directory completions
    - add alias finder to zsh configuration
    - update FPATH to include additional zsh function directories

--- Configs/zsh/.config/zsh/completion.sh
+++ Configs/zsh/.config/zsh/completion.sh
@@ -4,15 +4,31 @@
 # ---- Completion styling ---- #
 # disable sort when completing `git checkout`
 zstyle ':completion:*:git-checkout:*' sort false
+
 # set descriptions format to enable group support
 zstyle ':completion:*:descriptions' format '[%d]'
+
+# Case-insenstive tab completion
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+
+# Add colors to the default completion menu (which is replaced by fzf)
 zstyle ':completion:*' list-colors "${LS_COLORS}"
+
+# Disable default zsh completion menu
 zstyle ':completion:*' menu no
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --color=always --long --no-time --no-user --sort name --no-permissions --no-filesize --all --only-dirs $realpath'
-zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza --color=always --long --no-time --no-user --sort name --no-permissions --no-filesize --all --only-dirs $realpath'
+
+# Preview directories when using cd/zoxide completion
+_cdCompletions='eza --color=always --long --no-time --no-user --sort name --no-permissions --no-filesize --all $realpath'
+zstyle ':fzf-tab:complete:cd:*' fzf-preview "${_cdCompletions}"
+zstyle ':fzf-tab:complete:z:*' fzf-preview "${_cdCompletions}"
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview "${_cdCompletions}"
+
+#
 zstyle ':fzf-tab:complete:_files:*' fzf-preview 'eza --color=always --long --no-time --no-user --sort name --no-permissions --no-filesize --all $realpath'
 zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
+
+# Enable alias finder
+zstyle ':omz:plugins:alias-finder' autoload yes
 
 # ---- Load completions ---- #
 zstyle :compinstall filename "${ZSHRC}"
@@ -24,7 +40,7 @@ zinit cdreplay -q
 
 # ---- find the command plugin ---- #
 ftc='/usr/share/doc/find-the-command/ftc.zsh'
-[[ -s "${ftc}" ]] && source "${ftc}" noupdate
+[[ -s "${ftc}" ]] && source "${ftc}" quiet noupdate
 
 # ---- Enable better selection support ---- #
 autoload -Uz select-word-style
@@ -35,5 +51,7 @@ zstyle ':zle:*' region-highlight 'fg=none' 'bg=none'
 
 # ---- Add completions to search path ---- #
 if [[ ":${FPATH}:" != *":${ZSH_CONF}/completions:"* ]]; then
-  export FPATH="${ZSH_CONF}/completions:${FPATH}"
+  FPATH="${ZSH_CONF}/completions:${FPATH}"
 fi
+
+export FPATH="${FPATH}:/usr/share/zsh/functions:/usr/share/zsh/functions/Zle"
