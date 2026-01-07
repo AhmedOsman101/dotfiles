0bed94f53c7c0e3af9acb7041b9d5abbf37a8d70
Ahmad Othman <ahmad.ali.othman@outlook.com>
2026-01-07 14:41:25 +0200

    refactor(zsh): consolidate history settings
    - move history settings to dedicated file
    - remove redundant history configuration from .zshenv

--- Configs/zsh/.config/zsh/history.sh
+++ Configs/zsh/.config/zsh/history.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2034
 
 # ---- History ---- #
-setopt extended_history     # Enable timestamps in history
+HISTSIZE=999999
+SAVEHIST=${HISTSIZE}
+HISTFILE="${HOME}/.zsh_history"
+HISTDUP=erase
+HISTTIMEFORMAT="%F %T "
+HISTCONTROL="ignoreboth"
+setopt appendhistory        # Append command to history instead of overwriting
 setopt share_history        # Share history across shells
-setopt inc_append_history   # Add commands to history immediately
 setopt hist_ignore_space    # Ignore commands starting with space
 setopt hist_ignore_all_dups # Remove older duplicate entries
+setopt hist_ignore_dups     # Remove older duplicate entries
 setopt hist_save_no_dups    # Don't save duplicates to history file
 setopt hist_find_no_dups    # Skip duplicates when searching history
+setopt extended_history     # Enable timestamps in history
+setopt inc_append_history   # Add commands to history immediately
 
 # ---- Remove duplicate history entries ---- #
 command -v no-dups &>/dev/null && no-dups -f -q "${HISTFILE}"
