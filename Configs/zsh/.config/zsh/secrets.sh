295b06ba6c4ced192c9e09c1ead97c7d0a0e1845
Ahmad Othman <ahmad.ali.othman@outlook.com>
2026-01-13 02:25:38 +0200

    feat(aliases): add jq and jless global aliases
    - add alias for piping to jq
    - add alias for piping to jless

--- Configs/zsh/.config/zsh/secrets.sh
+++ Configs/zsh/.config/zsh/secrets.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-export GEMINI_API_KEY="$(pass show gemini)"
-export GOOGLE_GENERATIVE_AI_API_KEY="${GEMINI_API_KEY}"
+GEMINI_API_KEY="$(pass show gemini)"
+GOOGLE_GENERATIVE_AI_API_KEY="${GEMINI_API_KEY}"
 
-export OPENAI_API_KEY="$(pass show openai)"
+OPENAI_API_KEY="$(pass show openai)"
 
-export GITHUB_TOKEN="$(pass show github/tokens/1)"
+GITHUB_TOKEN="$(pass show github/tokens/1)"
 
-export ADVENT_OF_CODE_SESSION="$(pass show advent-of-code)"
+ADVENT_OF_CODE_SESSION="$(pass show advent-of-code)"
+
+ANTHROPIC_AUTH_TOKEN="$(pass show agent-router)"
+ANTHROPIC_API_KEY="$(pass show agent-router)"
+
+export GEMINI_API_KEY \
+  GOOGLE_GENERATIVE_AI_API_KEY \
+  OPENAI_API_KEY \
+  GITHUB_TOKEN \
+  ADVENT_OF_CODE_SESSION \
+  ANTHROPIC_AUTH_TOKEN \
+  ANTHROPIC_API_KEY
